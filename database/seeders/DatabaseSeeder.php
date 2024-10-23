7c61a3b9f197cf9eeb2e36735592a18db78c7d2c
Chadwin Kyle Fritz <100412900+ChadwinFritz@users.noreply.github.com>
2024-10-23 19:24:32 +0200

    third commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\LeaveTypeSeeder;
+use Database\Seeders\DepartmentSeeder;
+use Database\Seeders\DutySeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\EmployeeSeeder;
+use Database\Seeders\LeaveSeeder;
+use Database\Seeders\LeaveApplicationSeeder;
+use Database\Seeders\SystemSettingsSeeder;
+use Database\Seeders\AdminSeeder;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,13 +25,15 @@ class DatabaseSeeder extends Seeder
     {
         // Call individual seeders in the correct order
         $this->call([
-            LeaveTypeSeeder::class,     // 1. Leave Types
-            DepartmentSeeder::class,     // 2. Departments
-            DutySeeder::class,           // 3. Duties
-            UserSeeder::class,           // 4. Users
-            EmployeeSeeder::class,       // 5. Employees
-            LeaveSeeder::class,          // 6. Leaves
-            LeaveApplicationSeeder::class // 7. Leave Applications
+            LeaveTypeSeeder::class,       // 1. Leave Types
+            DepartmentSeeder::class,      // 2. Departments
+            DutySeeder::class,            // 3. Duties
+            UserSeeder::class,            // 4. Users
+            EmployeeSeeder::class,        // 5. Employees
+            LeaveSeeder::class,           // 6. Leaves
+            LeaveApplicationSeeder::class, // 7. Leave Applications
+            SystemSettingsSeeder::class,  // 8. System Settings
+            AdminSeeder::class            // 9. Admins
         ]);
     }
 }
